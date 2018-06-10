f9c9db5910f01a9637aeb51dad3de6c3bf643ce4
Kevin Huang <yellow.khaos@gmail.com>
2018-06-10 17:28:05 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,17 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    redirect student(@student)
+  end
+
   private
 
     def set_student
